0d2eee25151b4c8dc97176b143b0f352fcdc4fd2
mattfazio352-hash <mattfazio352@gmail.com>
2026-06-05 08:47:01 -0400

    Add theme colors for UI composition

---
+++ app/src/main/java/com/FanMessenGer/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.FanMessenGer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NeonOrange = Color(0xFFFF6600)
+val BrightBlue = Color(0xFF0066FF)
+val DarkOrange = Color(0xFFCC5500)
+val DarkBlue = Color(0xFF0052CC)
+val OrangeAccent = Color(0xFFFF8833)
+val BlueAccent = Color(0xFF3385FF)
+val CyanAccent = Color(0xFF00CCFF)
+
+val White = Color(0xFFFFFFFF)
+val LightGray = Color(0xFFF5F5F5)
+val MediumGray = Color(0xFFE0E0E0)
+val DarkGray = Color(0xFF424242)
+val Black = Color(0xFF000000)
+
+val SentMessageBg = NeonOrange
+val ReceivedMessageBg = LightGray
+val SentMessageText = White
+val ReceivedMessageText = Black
+
+val SuccessGreen = Color(0xFF4CAF50)
+val ErrorRed = Color(0xFFFF3B30)
+val WarningYellow = Color(0xFFFFC107)
+val SpamRed = Color(0xFFFF3B30)
+val SafeGreen = Color(0xFF4CAF50)
